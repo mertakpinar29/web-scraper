1e229f028359249f0387d40e5829a89de309233a
Mert Akpınar <mert.akpinar@hotmail.com>
2021-09-02 16:43:32 +0300

    first

---
+++ AOT/Controllers/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AOT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var url = "https://attackontitan.fandom.com/wiki/List_of_characters/Anime";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            IList<HtmlNode> nodes = doc.QuerySelectorAll("div.characterbox-main")[1]
+            .QuerySelectorAll("div.characterbox-container table tbody");
+
+            var data = nodes.Select((node) =>
+            {
+                var name = node.QuerySelector("tr:nth-child(2) th a").InnerText;
+                return new
+                {
+                    name = name,
+                    imageUrl = node.QuerySelector("tr td div a img")
+                    .GetAttributeValue("data-src", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F716487122224439296%2FHWPluyjs.jpg&f=1&nofb=1"),
+                    descriptionUrl = $"https://attackontitan.fandom.com/wiki/{name.Replace(" ", "_")}_(Anime)"
+                };
+            });
+
+            return Ok(data);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+ -[x]  Install dotnet SDK
+   * https://docs.microsoft.com/en-us/dotnet/core/install/windows
+
+ -[x] Instal C# for VS Code
+
+ -[x] Create a new web project with 
+   `dotnet new webapi -o AppName`
+   * https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio-code
+ 
+ -[x] Make a HTTP request to get the html.
+   * https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=net-5.0
+
+ -[x] Install HtmlAgilityPack and its CssSelector package to select elements using querySelector.
+   * https://www.nuget.org/packages/HtmlAgilityPack/
+   * https://www.nuget.org/packages/HtmlAgilityPack.CssSelectors.NetCore/
+   * https://html-agility-pack.net/
+
+-[x] Select all characters with QuerySelectorAll() method.
+   * https://html-agility-pack.net/knowledge-base/40689181/css-selector-in-dotnetcore-
